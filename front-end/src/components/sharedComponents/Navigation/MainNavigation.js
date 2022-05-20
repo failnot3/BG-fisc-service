f570d8dbe805d4d70bf349278f98a048c9213d06
DTS <dts@DTS-MacBook-Air.local>
2022-05-20 12:31:55 +0300

    changes

--- front-end/src/components/sharedComponents/Navigation/MainNavigation.js
+++ front-end/src/components/sharedComponents/Navigation/MainNavigation.js
@@ -35,7 +35,7 @@ const MainNavigation = (props) => {
           <span />
         </button>
         <h1 className='main-navigation__title'>
-          <Link to='/'>OpenBGPOS</Link>
+          <Link to='/'>BGFiscalService</Link>
         </h1>
         <nav className='main-navigation__header-nav'>
           <NavLinks />
