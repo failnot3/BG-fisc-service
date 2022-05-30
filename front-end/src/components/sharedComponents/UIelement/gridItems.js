713db5127818421e018fc888494877c4e6fcbc3a
theDTS1 <orel1one@gmail.com>
2022-05-30 23:52:43 +0300

    changes

--- front-end/src/components/sharedComponents/UIelement/gridItems.js
+++ front-end/src/components/sharedComponents/UIelement/gridItems.js
@@ -17,8 +17,11 @@ function myFunction1() {
 const GridItem = () => {
   return (
     <>
-      <Grid item xs={4}>
-        <Box padding={3} sx={{ textAlign: "center" }}>
+      <Grid item xs={6} md={4}>
+        <Box
+          padding={4}
+          sx={{ textAlign: "center", justifyContent: "space-evenly" }}
+        >
           <Typography variant='h6' component='h4'>
             <Button variant='contained' onClick={myFunction1}>
               Всички Фискални Устройства
@@ -26,8 +29,12 @@ const GridItem = () => {
           </Typography>
         </Box>
       </Grid>
-      <Grid item xs={4}>
-        <Box padding={3} sx={{ textAlign: "center" }}>
+
+      <Grid item xs={6} md={4}>
+        <Box
+          padding={4}
+          sx={{ textAlign: "center", justifyContent: "space-evenly" }}
+        >
           <Typography variant='h6' component='h4'>
             <Button variant='contained' onClick={myFunction}>
               Справки Фискални Устройства
@@ -35,29 +42,41 @@ const GridItem = () => {
           </Typography>
         </Box>
       </Grid>
-      <Grid item xs={4}>
-        <Box padding={3} sx={{ textAlign: "center" }}>
+      <Grid item xs={6} md={4}>
+        <Box
+          padding={4}
+          sx={{ textAlign: "center", justifyContent: "space-evenly" }}
+        >
           <Typography variant='h6' component='h4'>
             <Button variant='contained'>Изтичащи договори</Button>
           </Typography>
         </Box>
       </Grid>
-      <Grid item xs={4}>
-        <Box padding={3} sx={{ textAlign: "center" }}>
+      <Grid item xs={6} md={4}>
+        <Box
+          padding={4}
+          sx={{ textAlign: "center", justifyContent: "space-evenly" }}
+        >
           <Typography variant='h6' component='h4'>
             <Button variant='contained'>Заявка нова фирма</Button>
           </Typography>
         </Box>
       </Grid>
-      <Grid item xs={4}>
-        <Box padding={4} sx={{ textAlign: "center" }}>
+      <Grid item xs={6} md={4}>
+        <Box
+          padding={4}
+          sx={{ textAlign: "center", justifyContent: "space-evenly" }}
+        >
           <Typography variant='h6' component='h4'>
             <Button variant='contained'>Промяна по текуща фирма</Button>
           </Typography>
         </Box>
       </Grid>
-      <Grid item xs={4}>
-        <Box padding={4} sx={{ textAlign: "center" }}>
+      <Grid item xs={6} md={4}>
+        <Box
+          padding={4}
+          sx={{ textAlign: "center", justifyContent: "space-evenly" }}
+        >
           <Typography variant='h6' component='h4'>
             <Button variant='contained'>...</Button>
           </Typography>
